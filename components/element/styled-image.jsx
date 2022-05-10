aaa6d7a5e2b4345197c1eb898ede3ae377286e37
Mon Macbook <wendykrwn@gmail.com>
2022-05-10 19:54:52 +0200

    custom image style

--- components/element/styled-image.jsx
+++ components/element/styled-image.jsx
@@ -1,8 +1,11 @@
 import ButtonLink from "./buttonLink"
+import CustomImage from "./customImage"
 
-const StyledImage = ({ buttonLinks }) => {
-  console.log(buttonLinks)
+const StyledImage = (data) => {
+  console.log({data})
+ const { buttonLinks, customImages } = data
   return (
+    <div className="flex flex-col gap-y-5 mt-10">
     <div className="flex gap-x-3">
       {buttonLinks?.map(({text,url,newTab,color,type}) => {
         return (
@@ -15,6 +18,12 @@ const StyledImage = ({ buttonLinks }) => {
         />
         )
       })}
+      </div>
+      <div className="mt-5 flex flex-wrap gap-x-8">
+        {customImages?.map(({ image, color }) => <div className="w-[500px]">
+          <CustomImage image={image} color={color} />
+        </div>)}
+      </div>
     </div>
   )
 }
