3fbc57e98403bdc118776310159c62345ffedba5
Loïc Torres <loic.torresg@gmail.com>
2022-05-10 08:51:38 +0200

    fixed tailwind warnings

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,24 @@
 module.exports = {
+  plugins: [
+    "postcss-import",
+    { "tailwindcss/nesting": {} },
+    "tailwindcss",
+    "postcss-flexbugs-fixes",
+    "postcss-nesting",
+    "postcss-custom-properties",
+    "autoprefixer",
+  ],
+}
+
+// postcss.config.js
+module.exports = {
   plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
+    "postcss-flexbugs-fixes": {},
+    "postcss-nesting": {},
+    "postcss-custom-properties": {},
   },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
+const { colors } = require(`tailwindcss/defaultTheme`)
+
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: "jit", // see https://tailwindcss.com/docs/just-in-time-mode
+  content: ["./components/**/*.js", "./pages/**/*.js"],
   theme: {
+    extend: {
+      colors: {
+        primary: "crimson",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          md: "2rem",
+        },
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 }
