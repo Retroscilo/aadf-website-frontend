0163c00c00535b6baf8bf85b37ad92b64e4104fd
Mon Macbook <wendykrwn@gmail.com>
2022-05-10 15:53:46 +0200

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
-const { colors } = require(`tailwindcss/defaultTheme`)
+const { colors } = require(`tailwindcss/defaultTheme`);
 
 module.exports = {
   mode: "jit", // see https://tailwindcss.com/docs/just-in-time-mode
-  content: ["./components/**/*.js", "./pages/**/*.js"],
+  purge: ["./components/**/*.js", "./pages/**/*.js"],
+  darkMode: false, // or "media" or "class"
   theme: {
     extend: {
       colors: {
-        primary: "crimson",
+        primary: {
+          1: "#E51E31",
+          2: "#202020",
+        },
+        secondary: {
+          1: "#010E88",
+          2: "#FF9C00"
+        },
+        date: "#6B6B6B",
+        default: "#F9F9F9"
       },
       container: {
         center: true,
@@ -15,6 +25,16 @@ module.exports = {
           md: "2rem",
         },
       },
+    },
+    fontFamily: {
+      DosisRegular: ["DosisRegular","sans-serif"],
+      DosisBold: ["DosisBold","sans-serif"],
+      DosisSemiBold: ["DosisSemiBold","sans-serif"],
+      KarlaBold: ["KarlaBold","sans-serif"],
+      KarlaSemiBold: ["KarlaSemiBold","sans-serif"],
+      KarlaRegular: ["KarlaRegular","sans-serif"],
+      KarlaMedium: ["KarlaMedium","sans-serif"],
+
     },
     screens: {
       sm: "640px",
@@ -27,4 +47,4 @@ module.exports = {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
-}
+};
