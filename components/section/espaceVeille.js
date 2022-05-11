7b9515a86aeddc8884af142221bcdcdb367cba56
Mon Macbook <wendykrwn@gmail.com>
2022-05-12 00:42:28 +0200

    responsive veille

--- components/section/espaceVeille.js
+++ components/section/espaceVeille.js
@@ -4,8 +4,8 @@ import Veille from "../element/veille"
 const EspaceVeilles = ({ title, arrowLink, veilles }) => {
   return (
     <section className="flex flex-col">
-      <h2 className="text-center">{title}</h2>
-      <div className="flex w-full overflow-auto my-[72px] py-[20px] gap-x-[126px]">
+      <h2 className="ml-[2rem] lg:text-center">{title}</h2>
+      <div className="flex w-full overflow-auto my-[36px] lg:my-[72px]  py-[20px] gap-x-[35px] lg:gap-x-[126px]">
         {veilles?.data?.map((veille) => (
           <Veille {...veille.attributes} />
         ))}
