2401d6854ea9c6e43c53fb598b1d6c582fafc62b
Mon Macbook <wendykrwn@gmail.com>
2022-05-12 12:25:18 +0200

    police

--- components/section/presentationSection.js
+++ components/section/presentationSection.js
@@ -1,6 +1,7 @@
 import MarkdownToJsx from "../../utils/markdownToJsx"
 import ArrowLink from "../element/arrowLink"
 import CustomImage from "../element/customImage"
+import { useRouter } from "next/router"
 
 const PresentationSection = ({
   title,
@@ -9,11 +10,13 @@ const PresentationSection = ({
   imageFirst,
   arrowLink,
 }) => {
+//   const router = useRouter()
+//   console.log( router.basePath )
   return (
     <section className="">
       <h2 className="text-center">{title}</h2>
       <div className={`flex w-full container justify-between gap-x-[172px]`}>
-        <div className={`w-full  ${imageFirst ? "order-first" : "order-last"}`}>
+        <div className={`w-full  ${imageFirst ? "px-[35px] order-first" : "order-last"}`}>
           <CustomImage {...image} />
         </div>
         <div className={`w-full`}>
