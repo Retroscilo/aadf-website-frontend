eb2ca91f23a947dab4b15ee47f8ca8819ca1ec76
Loïc Torres <loic.torresg@gmail.com>
2022-05-12 16:50:50 +0200

    updated packages

--- utils/markdownToJsx.js
+++ utils/markdownToJsx.js
@@ -1,19 +1,21 @@
 import Markdown from "markdown-to-jsx"
 
 const MarkdownToJsx = ({ children }) => {
-    return (
-        <Markdown
-            options={{
-                overrides: {
-                    rouge: {
-                        component: ({ children }) => <span className=" text-primary-1 ">{children}</span>
-                    }
-                }
-            }}
-        >
-            {children}
-        </Markdown>
-    )
+  return (
+    <Markdown
+      options={{
+        overrides: {
+          rouge: {
+            component: ({ children }) => (
+              <span className=" text-primary-1 ">{children}</span>
+            ),
+          },
+        },
+      }}
+    >
+      {children}
+    </Markdown>
+  )
 }
 
 export default MarkdownToJsx
