cd2a026e2284dd78248b7a294b8ac42365754fa3
Mon Macbook <wendykrwn@gmail.com>
2022-05-13 00:40:30 +0200

    action resp

--- components/section/actions.js
+++ components/section/actions.js
@@ -4,14 +4,14 @@ import ArrowLink from "../element/arrowLink"
 const Actions = ({ title, actions, arrowLink }) => {
   return (
     <section className="md:container">
-      <h2>{title}</h2>
+      <h2 className="hidden underlined-primary  lg:flex">{title}</h2>
       <div className="w-full md:grid grid-cols-1 md:grid-cols-2 gap-y-0 gap-x-0 md:gap-x-[2rem] md:gap-y-[6.5rem]">
         {actions?.map((action) => (
           <Action {...action} />
         ))}
       </div>
       {arrowLink && (
-        <div className="flex md:justify-center justify-start m-0">
+        <div className="flex container md:justify-center justify-start m-0">
           <ArrowLink {...arrowLink} />
         </div>
       )}
