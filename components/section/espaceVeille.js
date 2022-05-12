533e9ba623fd4cd712be36f3a29c42e08b5e798c
Mon Macbook <wendykrwn@gmail.com>
2022-05-13 00:26:40 +0200

    espacement espaceVeille

--- components/section/espaceVeille.js
+++ components/section/espaceVeille.js
@@ -3,9 +3,9 @@ import Veille from "../element/veille"
 
 const EspaceVeilles = ({ title, arrowLink, veilles }) => {
   return (
-    <section className="flex flex-col">
+    <section>
       <h2 className="ml-[2rem] lg:text-center">{title}</h2>
-      <div className="flex w-full overflow-auto my-[36px] lg:my-[72px]  py-[20px] gap-x-[35px] lg:gap-x-[126px]">
+      <div className="flex w-full overflow-auto py-[2rem] gap-x-[35px] lg:gap-x-[126px]">
         {veilles?.data?.map((veille) => (
           <Veille {...veille.attributes} />
         ))}
