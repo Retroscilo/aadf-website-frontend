57671baebbf0dbb15564418be9bd2c8a6796b5c3
Mon Macbook <wendykrwn@gmail.com>
2022-05-13 00:05:31 +0200

    ajout arrowLink dans Actions

--- components/section/actions.js
+++ components/section/actions.js
@@ -1,14 +1,22 @@
 import Action from "../element/oneAction"
+import ArrowLink from "../element/arrowLink"
 
-const Actions = ({title,actions}) => {
-    return <div className="md:container">
-        <h2>{title}</h2>
-        <div className="w-full md:grid grid-cols-1 md:grid-cols-2 gap-y-0 gap-x-0 md:gap-x-[2rem] md:gap-y-[6.5rem]">
-            {
-                actions?.map(action => <Action {...action}/>)
-            }           
+const Actions = ({ title, actions, arrowLink }) => {
+  return (
+    <section className="md:container">
+      <h2>{title}</h2>
+      <div className="w-full md:grid grid-cols-1 md:grid-cols-2 gap-y-0 gap-x-0 md:gap-x-[2rem] md:gap-y-[6.5rem]">
+        {actions?.map((action) => (
+          <Action {...action} />
+        ))}
+      </div>
+      {arrowLink && (
+        <div className="flex md:justify-center justify-start m-0">
+          <ArrowLink {...arrowLink} />
         </div>
-    </div>
+      )}
+    </section>
+  )
 }
 
 export default Actions
