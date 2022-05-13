ba53d7f176b1af7bffceaef9ec77503f206f8597
Loïc Torres <loic.torresg@gmail.com>
2022-05-13 06:58:00 +0200

    fixed pricing section

--- components/element/pricingCard.jsx
+++ components/element/pricingCard.jsx
@@ -1,7 +1,7 @@
 const pricingCard = ({ data }) => {
   console.log(data)
   return (
-    <article className="relative pricingCard xl:max-w-[354px] xl:min-w-[354px] flex-grow xl:shadow-md grid xl:grid-rows-[50px_100px_1fr] xl:grid-cols-[1fr] grid-rows-[50px_100px] grid-cols-[1fr_100px] gap-10 p-14">
+    <article className="relative pricingCard xl:max-w-[354px] xl:min-w-[354px] flex-grow xl:shadow-md grid xl:grid-rows-[50px_100px_1fr] xl:grid-cols-[1fr] grid-rows-[auto_auto] grid-cols-[1fr_100px] gap-10 p-14">
       <h4 className="xl:text-center row-start-2 col-start-2 text-right xl:row-start-1 xl:col-start-1">
         {data.price}
       </h4>
