f21650cbd9f64ddbc3a3cdd09bb07549e17f77dd
Mon Macbook <wendykrwn@gmail.com>
2022-05-13 12:38:38 +0200

    add action url

--- components/element/oneAction.js
+++ components/element/oneAction.js
@@ -1,18 +1,21 @@
 import { getStrapiMedia } from "../../lib/media"
+import CustomLink from './CustomLink'
 
-const Action = ({ title, subtitle, image }) => {
+const Action = ({ title, subtitle, image, url }) => {
     return (
-        <div className={`w-full h-[22rem] md:h-[29rem] flex flex-col actionImage`}
-            style={{
-                backgroundImage: `url(${getStrapiMedia(image)})`,
-            }} >
-            <div className="inline-block bg-default py-[.6rem] px-[3rem] ml-auto">
-                {subtitle}
+        <CustomLink url={url} newTab={false}>
+            <div className={`w-full h-[22rem] md:h-[29rem] flex flex-col actionImage`}
+                style={{
+                    backgroundImage: `url(${getStrapiMedia(image)})`,
+                }} >
+                <div className="inline-block bg-default py-[.6rem] px-[3rem] ml-auto">
+                    {subtitle}
+                </div>
+                <div className="mt-auto text-white bg-black bg-opacity-[41%] p-[2.6rem]">
+                    {title}
+                </div>
             </div>
-            <div className="mt-auto text-white bg-black bg-opacity-[41%] p-[2.6rem]">
-                {title}
-            </div>
-        </div>
+        </CustomLink>
     )
 }
 
