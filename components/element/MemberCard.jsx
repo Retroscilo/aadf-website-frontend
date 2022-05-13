d003bf8d6d87dcf5ea04d978b080e52a4ea63e28
Loïc Torres <loic.torresg@gmail.com>
2022-05-13 15:03:25 +0200

    center

--- components/element/MemberCard.jsx
+++ components/element/MemberCard.jsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 const MemberCard = ({ member }) => {
   return (
     <div className="grid grid-rows-[4fr_.8fr_.5fr] justify-self-center shadow-lg max-w-[300px]">
-      <div className="h-[320px] w-full relative">
+      <div className="h-[320px] min-w-[300px] w-full relative">
         <Image
           src={getStrapiMedia(member.image)}
           blurDataURL={getStrapiMedia(member.image)}
