e1a39dd0351b9a0f1ed29900a5222baa18b8150a
Loïc Torres <loic.torresg@gmail.com>
2022-05-13 09:52:11 +0200

    missions fix

--- components/element/missionCard.jsx
+++ components/element/missionCard.jsx
@@ -2,12 +2,14 @@ import CustomImage from "./customImage"
 
 const MissionCard = ({ missionTitle, image, description }) => {
   return (
-    <div className="grid-rows-[100px_200px_1fr] xl:w-[50%]  w-[100%] inline-grid">
-      <div className="h-[300px] w-[200px] mx-auto">
+    <div className="grid-rows-[auto_auto_1fr] xl:w-[50%]  w-[100%] inline-grid gap-12 my-20 lg:px-20">
+      <h2 className="text-center lg:text-left lg:row-start-2">
+        {missionTitle}
+      </h2>
+      <div className="w-[200px] mx-auto lg:row-start-1 lg:mx-0">
         <CustomImage image={image.image} color={image.color} />
       </div>
-      <h2 className="text-center">{missionTitle}</h2>
-      <p className="max-w-[500px] text-center justify-self-center">
+      <p className="max-w-[500px] justify-self-center lg:justify-self-start lg:row-start-3">
         {description}
       </p>
     </div>
