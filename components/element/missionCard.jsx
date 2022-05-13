e45182784c5b830ca1e86e7c55158b30eb86a827
Loïc Torres <loic.torresg@gmail.com>
2022-05-13 14:18:34 +0200

    fixed missionCard padding

--- components/element/missionCard.jsx
+++ components/element/missionCard.jsx
@@ -2,11 +2,11 @@ import CustomImage from "./customImage"
 
 const MissionCard = ({ missionTitle, image, description }) => {
   return (
-    <div className="grid-rows-[auto_auto_1fr] xl:w-[50%] lg:pl-30  w-[100%] inline-grid gap-12 my-20 lg:px-20">
+    <div className="grid-rows-[auto_auto_1fr] xl:w-[50%] lg:pl-40 w-[100%] inline-grid gap-12 my-20 lg:px-20">
       <h2 className="text-center lg:text-left lg:row-start-2">
         {missionTitle}
       </h2>
-      <div className="w-[200px] mx-auto lg:row-start-1 lg:mx-0">
+      <div className=" w-[300px] lg:w-[420px] mx-auto lg:row-start-1 lg:mx-0">
         <CustomImage image={image.image} color={image.color} />
       </div>
       <p className="max-w-[500px] justify-self-center lg:justify-self-start lg:row-start-3">
